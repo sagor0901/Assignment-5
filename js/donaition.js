0f82cfa5725f6eb59ee3e0ba4488a5b2bec89533
sagor0901 <md.tanjim28602@gmail.com>
2024-09-22 21:58:30 -0700

    index.html

--- js/donaition.js
+++ js/donaition.js
@@ -1,18 +1,60 @@
-document.getElementById('donate-now-btn').addEventListener('click', function(event){
-    event.preventDefault();
+// frist section donation function
+document.getElementById('donate-now-btn').addEventListener('click', function(){
+    // event.preventDefault();
     const inputAmount = getInputValueById('input-amount-field');
     
     const mainBalance = getTextValueById('my-balance');
     // console.log(inputAmount, amount);
     
-    if(inputAmount < 0 || isNaN(inputAmount)){
+    if(inputAmount < 0 || isNaN(inputAmount) || inputAmount >mainBalance){
         alert('valid a perfect amount');
     }
     else{
         const donation = mainBalance - inputAmount;
         document.getElementById('my-balance').innerText = donation;
         const donatedBalance = getTextValueById('donated-balance')
-        const donationAdd = donation + donatedBalance;
-        document.getElementById('donated-balance').innerText = inputAmount;
+        const donationAdd = inputAmount + donatedBalance;
+        document.getElementById('donated-balance').innerText = donationAdd;
+    }
+});
+
+// secound section donation function 
+
+document.getElementById('flood-btn').addEventListener('click', function(){
+    // event.preventDefault();
+    const inputAmount = getInputValueById('input-amount');
+    
+    const mainBalance = getTextValueById('my-balance');
+    // console.log(inputAmount, amount);
+    
+    if(inputAmount < 0 || isNaN(inputAmount) || inputAmount >mainBalance){
+        alert('valid a perfect amount');
+    }
+    else{
+        const donation = mainBalance - inputAmount;
+        document.getElementById('my-balance').innerText = donation;
+        const donatedBalance = getTextValueById('donated-balance-feni')
+        const donationAdd = inputAmount + donatedBalance;
+        document.getElementById('donated-balance-feni').innerText = donationAdd;
+    }
+});
+
+// third section donation function 
+document.getElementById('quota-btn').addEventListener('click', function(){
+    // event.preventDefault();
+    const inputAmount = getInputValueById('input-amount-for-studend');
+    
+    const mainBalance = getTextValueById('my-balance');
+    // console.log(inputAmount, amount);
+    
+    if(inputAmount < 0 || isNaN(inputAmount) || inputAmount >mainBalance){
+        alert('valid a perfect amount');
+    }
+    else{
+        const donation = mainBalance - inputAmount;
+        document.getElementById('my-balance').innerText = donation;
+        const donatedBalance = getTextValueById('donated-balance-student')
+        const donationAdd = inputAmount + donatedBalance;
+        document.getElementById('donated-balance-student').innerText = donationAdd;
     }
 });
